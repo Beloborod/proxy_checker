c22bf359ec71b3d7f3bf783e5991425cfa493b8b
beloborod <strig-style@yandex.ru>
2025-01-15 13:48:03 +0300

    mongo connecter

--- models/connecter.py
+++ models/connecter.py
@@ -3,10 +3,10 @@ from mongoengine import connect
 db_name = "YOUR_DB_NAME"
 
 def connection():
-    connect(db_name,
-            host="HOST_NAME",
-            username="USER_NAME",
-            password="USER_PASSWORD",
-            authentication_source='AUTHENTICATION_DB',
-            port=0 #YOUR PORT
-            )
+    return connect(db_name,
+                   host="HOST_NAME",
+                   username="USER_NAME",
+                   password="USER_PASSWORD",
+                   authentication_source='AUTHENTICATION_DB',
+                   port=0  # Your port
+                   )
