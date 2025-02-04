a3af3a1a2767a36568791feadf33fb3949581b16
beloborod <strig-style@yandex.ru>
2025-02-04 21:43:41 +0300

    fix main :D

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ from proxy_wrappers.best_proxies import get_proxies_best_proxies
 
 if __name__ == "__main__":
     setup_logger()
-    # proxy_collection = ProxyCollection()
-    #
-    # """--- GET NEW PROXIES ---"""
-    # for proxy in get_proxies_free_proxy():
-    #     proxy_collection.add_proxy(proxy)
-    #
-    # for proxy in get_proxies_geonode():
-    #     proxy_collection.add_proxy(proxy)
-    #
-    # """--- UPDATE SAVED PROXIES ---"""
-    # proxy_collection.load_from_mongo()
-    #
-    #
-    # """--- VALIDATE ALL ---"""
-    # proxy_collection.validate_all(multiprocess=True, with_web_driver=False, sync_mongo=True)
-    get_proxies_best_proxies()
+    proxy_collection = ProxyCollection()
+
+    """--- GET NEW PROXIES ---"""
+    for proxy in get_proxies_free_proxy():
+        proxy_collection.add_proxy(proxy)
+
+    for proxy in get_proxies_geonode():
+        proxy_collection.add_proxy(proxy)
+
+    for proxy in get_proxies_best_proxies():
+        proxy_collection.add_proxy(proxy)
+
+    """--- UPDATE SAVED PROXIES ---"""
+    proxy_collection.load_from_mongo()
+
+
+    """--- VALIDATE ALL ---"""
+    proxy_collection.validate_all(multiprocess=True, with_web_driver=False, sync_mongo=True)
