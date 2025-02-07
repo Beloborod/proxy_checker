969e925d8ba902101957022568db734c9027f5f5
beloborod <strig-style@yandex.ru>
2025-02-07 15:22:42 +0300

    try to fix leak by manual clear cache

--- daemon-main.py
+++ daemon-main.py
@@ -1,6 +1,6 @@
 import sys
 from time import sleep
-
+import streamlit
 from proxy_wrappers.thespeedx import get_proxies_thespeedx
 from src.proxy import ProxyCollection
 from src.logger import setup_logger
@@ -98,6 +98,7 @@ while True:
                                       args.max_workers, args.mongo_drop, args.judge)
 
         proxy_collection.cleanup()
+        st.legacy_caching.clear_cache()
         sleep(args.sleep)
     except KeyboardInterrupt:
         sys.exit(0)
